96f7e6f95a0e49cb09a003c3468a511d67ccae2f
SunTroll <50963945+Sun-Troll@users.noreply.github.com>
2020-12-11 23:35:06 +0100

    Circ implemented

---
+++ Engine/Circ.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Vec.h"
+
+template<typename T>
+class Circ_
+{
+public:
+	constexpr Circ_(Vec_<T> center_in, T radius_in)
+		:
+		center(center_in),
+		radius(radius_in)
+	{}
+	bool Coliding(const Circ_<T>& circ) const
+	{
+		return (center - circ.center).GetLengthSq() < (radius + circ.radius) * (radius + circ.radius);
+	}
+	bool ContainsCirc(const Circ_<T>& circ) const
+	{
+		return (center - circ.center).GetLength() + circ.radius < radius;
+	}
+	bool ContainsPoint(const Vec_<T>& point) const
+	{
+		return (center - point).GetLengthSq() < radius * radius;
+	}
+public:
+	Vec_<T> center;
+	T radius;
+};
+
+typedef Circ_<float> CircF;
+typedef Circ_<int> CircI;
