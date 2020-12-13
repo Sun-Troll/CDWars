d6546c89c6c363d03862590c9e2b7e479a1282ff
SunTroll <50963945+Sun-Troll@users.noreply.github.com>
2020-12-13 20:33:50 +0100

    chiliSexy code in

--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -24,8 +24,9 @@
 #include <wrl.h>
 #include "ChiliException.h"
 #include "Colors.h"
+#include "Surface.h"
 #include "Rect.h"
-#include "Circ.h"
+#include <cassert>
 
 class Graphics
 {
@@ -33,7 +34,7 @@ public:
 	class Exception : public ChiliException
 	{
 	public:
-		Exception( HRESULT hr,const std::wstring& note,const wchar_t* file,unsigned int line );
+		Exception(HRESULT hr, const std::wstring& note, const wchar_t* file, unsigned int line);
 		std::wstring GetErrorName() const;
 		std::wstring GetErrorDescription() const;
 		virtual std::wstring GetFullMessage() const override;
@@ -45,22 +46,70 @@ private:
 	// vertex format for the framebuffer fullscreen textured quad
 	struct FSQVertex
 	{
-		float x,y,z;		// position
-		float u,v;			// texcoords
+		float x, y, z;		// position
+		float u, v;			// texcoords
 	};
 public:
-	Graphics( class HWNDKey& key );
-	Graphics( const Graphics& ) = delete;
-	Graphics& operator=( const Graphics& ) = delete;
+	Graphics(class HWNDKey& key);
+	Graphics(const Graphics&) = delete;
+	Graphics& operator=(const Graphics&) = delete;
 	void EndFrame();
 	void BeginFrame();
-	void PutPixel( int x,int y,int r,int g,int b )
+	Color GetPixel(int x, int y) const;
+	void PutPixel(int x, int y, int r, int g, int b)
 	{
-		PutPixel( x,y,{ unsigned char( r ),unsigned char( g ),unsigned char( b ) } );
+		PutPixel(x, y, { unsigned char(r),unsigned char(g),unsigned char(b) });
 	}
-	void PutPixel( int x,int y,Color c );
-	void DrawRect(const RectI& rect, Color c);
-	void DrawCirc(const CircI& circ, Color c);
+	void PutPixel(int x, int y, Color c);
+	template<typename E>
+	void DrawSprite(int x, int y, const Surface& s, E effect)
+	{
+		DrawSprite(x, y, s.GetRect(), s, effect);
+	}
+	template<typename E>
+	void DrawSprite(int x, int y, const RectI& srcRect, const Surface& s, E effect)
+	{
+		DrawSprite(x, y, srcRect, GetScreenRect(), s, effect);
+	}
+	template<typename E>
+	void DrawSprite(int x, int y, RectI srcRect, const RectI& clip, const Surface& s, E effect)
+	{
+		assert(srcRect.left >= 0);
+		assert(srcRect.right <= s.GetWidth());
+		assert(srcRect.top >= 0);
+		assert(srcRect.bottom <= s.GetHeight());
+		if (x < clip.left)
+		{
+			srcRect.left += clip.left - x;
+			x = clip.left;
+		}
+		if (y < clip.top)
+		{
+			srcRect.top += clip.top - y;
+			y = clip.top;
+		}
+		if (x + srcRect.GetWidth() > clip.right)
+		{
+			srcRect.right -= x + srcRect.GetWidth() - clip.right;
+		}
+		if (y + srcRect.GetHeight() > clip.bottom)
+		{
+			srcRect.bottom -= y + srcRect.GetHeight() - clip.bottom;
+		}
+		for (int sy = srcRect.top; sy < srcRect.bottom; sy++)
+		{
+			for (int sx = srcRect.left; sx < srcRect.right; sx++)
+			{
+				effect(
+					s.GetPixel(sx, sy),
+					x + sx - srcRect.left,
+					y + sy - srcRect.top,
+					*this
+				);
+			}
+		}
+	}
+
 	~Graphics();
 private:
 	Microsoft::WRL::ComPtr<IDXGISwapChain>				pSwapChain;
@@ -79,5 +128,16 @@ private:
 public:
 	static constexpr int ScreenWidth = 800;
 	static constexpr int ScreenHeight = 600;
-	static constexpr RectI ScreenRect{ 0, ScreenWidth, 0, ScreenHeight };
+	static RectI GetScreenRect();
 };
+
+#include "SpriteEffect.h"
+
+#ifndef GOD_GRAPHICS
+extern template
+void Graphics::DrawSprite<SpriteEffect::Copy>(int x, int y, RectI srcRect, const RectI& clip, const Surface& s, SpriteEffect::Copy effect);
+extern template
+void Graphics::DrawSprite<SpriteEffect::Chroma>(int x, int y, RectI srcRect, const RectI& clip, const Surface& s, SpriteEffect::Chroma effect);
+extern template
+void Graphics::DrawSprite<SpriteEffect::Substitution>(int x, int y, RectI srcRect, const RectI& clip, const Surface& s, SpriteEffect::Substitution effect);
+#endif
